4097ed20ff495d8a10b33dfcccafa0ef70d263cf
Dan Walker <dwalker109@gmail.com>
2023-09-25 15:30:28 +0100

    Completed Challenge #2: Unique ID Generation

---
+++ c2-unique-id-generation/src/main.rs
@@ -0,0 +1,43 @@
+use maelstrom_rs::{make_reply, Body, Message, Node, Workload};
+use serde::{Deserialize, Serialize};
+use tokio::io::{stdin, stdout};
+use tokio::sync::mpsc::Sender;
+
+#[tokio::main]
+async fn main() {
+    let mut node = Node::startup(stdin(), stdout()).await.unwrap();
+    let workload = UniqueIdWorkload {};
+    node.run(workload).await;
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
+#[serde(tag = "type")]
+enum UniqueIdType {
+    Generate,
+    GenerateOk { id: String },
+}
+
+struct UniqueIdWorkload;
+
+#[async_trait::async_trait]
+impl Workload<UniqueIdType> for UniqueIdWorkload {
+    async fn handle(&self, message: Message<UniqueIdType>, tx: Sender<Message<UniqueIdType>>) {
+        let future = match message.data().to_owned() {
+            UniqueIdType::Generate => {
+                async move {
+                    let reply = make_reply(
+                        message,
+                        Body::new(UniqueIdType::GenerateOk {
+                            id: uuid::Uuid::new_v4().to_string(),
+                        }),
+                    );
+                    tx.send(reply).await.ok();
+                }
+            }
+            _ => unimplemented!(),
+        };
+
+        tokio::spawn(future);
+    }
+}
